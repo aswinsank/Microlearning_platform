981127b1c7858290115edff4ff242fd03c5b8aec
purnimacs <119433630+purnimacs@users.noreply.github.com>
2025-07-31 18:35:13 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,26 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import lessons
 from fastapi.staticfiles import StaticFiles
 import os
-from routes import auth
-
-
-
+from routes import lessons, quiz, text_lessons
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Update later for production
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(auth.router, prefix="/api")
-app.include_router(lessons.router, prefix="/api")
+
+app.include_router(lessons.router, prefix="/api")  
+app.include_router(quiz.router, prefix="/api")     
+app.include_router(text_lessons.router, prefix="/api")  
+
 os.makedirs("uploaded_videos", exist_ok=True)
 os.makedirs("uploaded_quiz", exist_ok=True)
+os.makedirs("uploaded_texts", exist_ok=True)  
 
 app.mount("/uploaded_videos", StaticFiles(directory="uploaded_videos"), name="videos")
 app.mount("/uploaded_quiz", StaticFiles(directory="uploaded_quiz"), name="quiz")
+app.mount("/uploaded_texts", StaticFiles(directory="uploaded_texts"), name="texts") 
+
+@app.get("/")
+async def root():
+    return {"message": "Microlearning Platform API"}
